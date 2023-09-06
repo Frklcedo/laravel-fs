ffd404b3031576c155ef46d738bfab46d8818b99
Frklcedo <franklinmacedo5115@gmail.com>
2023-09-06 00:37:29 -0300

    Learning Routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main page';
 });
+
+Route::get('/hello', function() {
+    return 'Hello';
+})->name('hello');
+Route::get('/greet/{name}', function($name) {
+    return "Hello, $name!";
+});
+
+Route::get('/hallo', function(){
+    return redirect()->route('hello');
+});
+
+Route::fallback(function(){
+    return 'Still got somewhere';
+});
+
+// GET
+// POST
+// PUT
+// DELETE
