da7376da8a5ffd0c1f0737e2e86b2a3f82f342dc
Frklcedo <franklinmacedo5115@gmail.com>
2023-09-09 23:00:04 -0300

    Forms, validation, storage, errors and flash msgs

--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8" />
     <title>Laravel 10 Task List App</title>
+    @yield('styles')
 </head>
 
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if(session()->has('success'))
+        <div>
+            {{ session('success') }}
+        </div>
+        @endif
         @yield('content')
     </div>
 </body>
