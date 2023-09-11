f8c93043e0342f543f26f015c78db6e8aa98ddf5
Frklcedo <franklinmacedo5115@gmail.com>
2023-09-11 00:54:05 -0300

    Blade include

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -1,54 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Add task')
-
-@section('styles')
-<style type="text/css" media="screen">
- .error-message{
-     color: red;
-     font-size: 0.8rem;
- }
-</style>
-
-@endsection
-
 @section('content')
-
-{{-- $errors --}}
-<form method="post" id="" action="{{ route('tasks.store') }}">
-    @csrf
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" value="{{ old('title') }}" id="title" />
-        @error('title')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5" cols="" tabindex="">{{ old('description') }}</textarea>
-        @error('description')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-    <div>
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description" rows="10" cols="" tabindex="">{{ old('long_description') }}</textarea>
-        @error('long_description')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-
-    <div>
-        <button type="submit">Add task</button>
-    </div>
-
-</form>
-
+    @include('form')
 @endsection
--- l10-task-list/resources/views/edit.blade.php
+++ l10-task-list/resources/views/edit.blade.php
@@ -1,56 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Edit task')
-
-@section('styles')
-<style type="text/css" media="screen">
- .error-message{
-     color: red;
-     font-size: 0.8rem;
- }
-</style>
-
-@endsection
-
 @section('content')
-
-{{-- $errors --}}
-{{-- <form method="POST" id="" action="{{ route('tasks.update', ['id' => $task->id]) }}"> --}}
-<form method="POST" id="" action="{{ route('tasks.update', ['task' => $task->id]) }}">
-    @csrf
-    @method('PUT')
-    <div>
-        <label for="title">Title</label>
-        <input type="text" name="title" value="{{ $task->title }}" id="title" />
-        @error('title')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-    <div>
-        <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5" cols="" tabindex="">{{ $task->description }}</textarea>
-        @error('description')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-    <div>
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description" rows="10" cols="" tabindex="">{{ $task->long_description }}</textarea>
-        @error('long_description')
-        <p class="error-message">
-            {{ $message }}
-        </p>
-        @enderror
-    </div>
-
-    <div>
-        <button type="submit">Edit task</button>
-    </div>
-
-</form>
-
+    {{-- @include('form', ['task' => $task]) --}}
+    @include('form', ['task' => $task])
 @endsection
