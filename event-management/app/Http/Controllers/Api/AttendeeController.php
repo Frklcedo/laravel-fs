c835367d067ec30c4fbd6f12665717d0d440068d
Frklcedo <franklinmacedo5115@gmail.com>
2023-12-03 21:13:04 -0300

    Loading traits to attendees

--- event-management/app/Http/Controllers/Api/AttendeeController.php
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+
+    use CanLoadRelationships;
+
+    private $relations = [ 'user' ];
+
     /**
      * Display a listing of the resource.
      */
+
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -25,13 +34,14 @@ class AttendeeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
     public function store(Request $request, Event $event)
     {
         $attendee = $event->attendees()->create([
             'user_id' => 1,
          ]);
 
-        return new AttendeeResource($attendee);
+        return new AttendeeResource($this->loadRelationships( $attendee ));
 
     }
 
@@ -40,7 +50,7 @@ class AttendeeController extends Controller
      */
     public function show(string $event, Attendee $attendee)
     {
-        return AttendeeResource::collection([$attendee]);
+        return new AttendeeResource($this->loadRelationships( $attendee ));
     }
 
     /**
