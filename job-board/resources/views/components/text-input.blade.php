4c14f5a54ab7d827370d2fee088fdcfb3f8561b1
Frklcedo <franklinmacedo5115@gmail.com>
2023-12-17 19:39:57 -0300

    Using alpine to clearing text form field

--- job-board/resources/views/components/text-input.blade.php
+++ job-board/resources/views/components/text-input.blade.php
@@ -1,11 +1,13 @@
 <div class="relative">
-    @if($formId)
-    <button class="absolute top-0 right-0 bottom-0 px-2" onclick="getElementById('{{ $name }}').value = ''; getElementById('{{ $formId }}').submit()" type="button">
+    @if($formRef)
+    <button class="absolute top-0 right-0 bottom-0 px-2" type="button"
+            @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit()"
+    >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-slate-500">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
         </svg>
     </button>
     @endif
-    <input type="text" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}" placeholder="{{ $placeholder }}" class="w-full rounded-md border-0 py-1.5 pl-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
+    <input x-ref="input-{{ $name }}" type="text" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}" placeholder="{{ $placeholder }}" class="w-full rounded-md border-0 py-1.5 pl-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
 
 </div>
