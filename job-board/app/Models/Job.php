0e5960c3c17a7b62ec3cd0bea818e14c0aaeaeb9
Frklcedo <franklinmacedo5115@gmail.com>
2024-01-11 16:54:39 -0300

    creating JobApplication Model && migrations

--- job-board/app/Models/Job.php
+++ job-board/app/Models/Job.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
@@ -15,52 +15,54 @@ class Job extends Model
     public static array $experience = [
         'entry',
         'intermediate',
-        'senior'
+        'senior',
     ];
 
     public static array $category = [
         'IT',
         'Finance',
         'Sales',
-        'Marketing'
+        'Marketing',
     ];
 
-    public function employer() : BelongsTo
+    public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function jobApplications(): hasMany
+    {
+        return $this->hasMany(JobApplication::class);
     }
 
     public function scopeFilter(Builder|QueryBuilder $query, array $filters): Builder|QueryBuilder
     {
 
-        return $query->when($filters['search'] ?? null, function( $query, $search ){
+        return $query->when($filters['search'] ?? null, function ($query, $search) {
 
-            $query->where(function($query) use( $search )
-            {
-                $query->where('title', 'like', '%' . $search . '%')
-                      ->orWhere('description', 'like', '%' . $search . '%')
-                      ->orWhereHas(
-                          'employer',
-                          function( $query ) use( $search )
-                          {
-                              $query->where('company_name', 'like', '%' . $search . '%');
-                          }
-                      );
+            $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%'.$search.'%')
+                    ->orWhere('description', 'like', '%'.$search.'%')
+                    ->orWhereHas(
+                        'employer',
+                        function ($query) use ($search) {
+                            $query->where('company_name', 'like', '%'.$search.'%');
+                        }
+                    );
             });
 
-        })->when($filters['min_salary'] ?? null, function( $query, $minSalary ){
+        })->when($filters['min_salary'] ?? null, function ($query, $minSalary) {
             $query->where('salary', '>=', $minSalary);
 
-        })->when($filters['max_salary'] ?? null, function( $query, $maxSalary ){
+        })->when($filters['max_salary'] ?? null, function ($query, $maxSalary) {
             $query->where('salary', '<=', $maxSalary);
 
-        })->when($filters['experience'] ?? null, function( $query, $experience ){
+        })->when($filters['experience'] ?? null, function ($query, $experience) {
             $query->where('experience', $experience);
 
-        })->when($filters['category'] ?? null, function( $query, $category ){
+        })->when($filters['category'] ?? null, function ($query, $category) {
             $query->where('category', $category);
 
         });
     }
-
 }
