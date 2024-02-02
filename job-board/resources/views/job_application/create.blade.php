5778aa14dc937e861ee65f1a4e41128a31cb4d79
Frklcedo <franklinmacedo5115@gmail.com>
2024-02-02 18:14:54 -0300

    File upload

--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -8,11 +8,16 @@
         <h2 class="mb-4 text-lg font-medium">
             Your job application
 
-            <form action="{{ route('job.application.store', $job) }}" method="POST">
+            <form action="{{ route('job.application.store', $job) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-4">
                     <label class="block mb-2 text-sm text-slate-900 font-medium" for="expected_salary">Expected Salary</label>
                     <x-text-input type="number" name="expected_salary" />
+                </div>
+
+                <div class="mb-4">
+                    <label for="cv" class="block mb-2 text-sm text-slate-900 font-medium">Upload CV</label>
+                    <x-text-input type="file" name="cv" />
                 </div>
                 <x-button class="w-full">Apply</x-button>
             </form>
