89f47cf6d3e5dda51203655dbdf31bfd5a3b7a55
Frklcedo <franklinmacedo5115@gmail.com>
2024-02-06 14:15:52 -0300

    Listing jobs

--- job-board/resources/views/my_job/create.blade.php
+++ job-board/resources/views/my_job/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-breadcrumbs :links="['My Jobs' => route('my-jobs.index'), 'Create' => '#']" class="mb-8" />
+    <x-breadcrumbs :links="['My Jobs' => route('my-jobs.index'), 'Create' => '#']" class="mb-4" />
         <x-card class="mb-8">
             <form action="{{route('my-jobs.store')}}" method="POST">
                 @csrf
